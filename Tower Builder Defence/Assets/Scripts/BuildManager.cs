d43db0120f337d5459283f093647abaf64bcc112
Muhammet Emin Gürzoğlu <gurzoglum@gmail.com>
2024-04-12 00:59:43 +0300

    Initial

---
+++ Tower Builder Defence/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private GameObject cursor;
+    [SerializeField] private GameObject wood;
+    
+    
+
+    private void Update()
+    {
+        Cursor.visible = false;
+        cursor.transform.position = GetMousePos();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(wood, GetMousePos(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMousePos()
+    {
+        Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        return mousePos;
+    }
+}
